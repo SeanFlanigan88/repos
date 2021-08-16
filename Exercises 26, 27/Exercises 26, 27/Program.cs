974e30a0c443d4b9240c268124a69c8df7c81881
Sean Flanigan <seanflaniganjr6588@gmail.com>
2021-08-16 13:04:38 -0400

    Exercise 26, 27

---
+++ Exercises 26, 27/Exercises 26, 27/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Exercises_26__27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 26
+            string response = null;
+            int total = 0;
+
+            do
+            {
+                Console.WriteLine("Please enter some text :");
+                string text = Console.ReadLine().ToLower();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u')
+                    {
+                        total++;
+                    }
+                }
+
+                Console.WriteLine($"Total number of Vowels is {total}");
+
+                Console.WriteLine("Continue y/n ?");
+                response = Console.ReadLine();
+            }
+            while (response == "y");
+            if (response == "n")
+            {
+                Console.WriteLine("Goodbye !");
+            }
+
+            ////Exercise 27
+            //string response = null;
+            //int total = 0;
+            //do
+            //{
+            //    Console.WriteLine("Please enter some text :");
+            //    string text = Console.ReadLine().ToLower();
+
+            //    for (int i = 0; i < text.Length; i++)
+            //    {
+            //        if (text[i] != 'a' && text[i] != 'e' && text[i] != 'i' && text[i] != 'o' && text[i] != 'u' && text[i] != ' ')
+            //        {
+            //            total++;
+            //        }
+            //    }
+
+            //    Console.WriteLine($"Total number of Consonants is {total}");
+
+            //    Console.WriteLine("Would you like to continue y/n ?");
+            //    response = Console.ReadLine();
+
+            //} while (response == "y");
+            //if (response == "n")
+            //{
+            //    Console.WriteLine("Goodbye !");
+            //}
+
+
+        }
+    }
+}
---
+++ Mock Assessment 1/Mock Assessment 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mock_Assessment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsTheSame(int num1, int num2)
+        {
+           
+            if(num1 == num2)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public static double Subtract()
+        {
+            
+        }
+
+        public static int FindBuildingType(int buildType)
+        {
+
+        }
+    }
+}
