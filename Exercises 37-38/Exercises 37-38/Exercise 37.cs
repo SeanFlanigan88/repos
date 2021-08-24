22f6af017ee610976edfebec3144b8e62c354500
Sean Flanigan <seanflaniganjr6588@gmail.com>
2021-08-24 11:18:13 -0500

    Exercise 37 and 38 final

---
+++ Exercises 37-38/Exercises 37-38/Exercise 37.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercises_37_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response = null;
+            do
+            {
+                double[] array = new double[5];
+
+                Console.WriteLine("Please enter 5 numbers :");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = Convert.ToDouble(Console.ReadLine());
+                }
+
+                double sum = 0;
+
+                foreach (double d in array)
+                {
+                    sum += d;
+                }
+                Console.WriteLine(sum);
+
+                Console.WriteLine("Would you like to continue y/n ?");
+                response = Console.ReadLine();
+            } while (response == "y");
+        }
+    }
+}
---
+++ Exercises 37-38/Exercises 37-38/Exercise 38.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_37_38
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            string response = null;
+            do
+            {
+                double[] array = new double[5];
+
+                Console.WriteLine("Please enter 5 numbers :");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = Convert.ToDouble(Console.ReadLine());
+                }
+
+                double sum = 0;
+
+                foreach (double d in array)
+                {
+                    sum += d;
+                }
+                Console.WriteLine(sum / 5);
+
+                Console.WriteLine("Would you like to continue y/n ?");
+                response = Console.ReadLine();
+            } while (response == "y");
+        }
+    }
+}
