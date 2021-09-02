9c28fb81ceaf26377ebdd74675bef3f1ca4f4e41
Sean Flanigan <seanflaniganjr6588@gmail.com>
2021-09-02 17:09:37 -0500

    First edition 49 and 50

---
+++ Exercises 49 and 50/Exercises 49 and 50/Exercise 50.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Exercises_49_and_50
+{
+    class Exercise_50
+    {
+        static void Main(string[] args)
+        {
+            List<string> numberInput = new List<string>();
+
+            Console.WriteLine("Please enter a side length");
+            string input = Console.ReadLine();
+
+            numberInput.Add(input);
+            //int maxInput = int.Parse(numberInput.Max());
+            //int minInput = int.Parse(numberInput.Min());
+
+            while (input != "q")
+            {
+                Console.WriteLine("Please enter a side length (q to quit)");
+                input = Console.ReadLine();
+
+                if (input != "q")
+                {
+                    numberInput.Add(input);
+                }
+            }
+
+            int maxInput = int.Parse(numberInput.Max());
+            int minInput = int.Parse(numberInput.Min());
+            int area = maxInput * maxInput;
+            int perimeter = maxInput * 4;
+
+            if (input == "q")
+            {
+                Console.WriteLine($"Largest : {maxInput} Smallest : {minInput} Largest Area : {area} Largest Perimeter : {perimeter}");
+            }
+        }
+    }
+}
---
+++ Exercises 49 and 50/Exercises 49 and 50/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercises_49_and_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numberInput = new List<string>();
+
+            Console.WriteLine("Please enter a side length");
+            string input = Console.ReadLine();
+
+            numberInput.Add(input);
+            //int maxInput = int.Parse(numberInput.Max());
+            //int minInput = int.Parse(numberInput.Min());
+
+            while (input != "q")
+            {
+                Console.WriteLine("Please enter a side length (q to quit)");
+                input = Console.ReadLine();
+
+                if(input != "q")
+                {
+                    numberInput.Add(input);
+                }
+            }
+
+            int maxInput = int.Parse(numberInput.Max());
+            int minInput = int.Parse(numberInput.Min());
+            int area = maxInput * maxInput;
+            int perimeter = maxInput * 4;
+
+            if (input == "q")
+            {
+                Console.WriteLine($"Largest : {maxInput} Smallest : {minInput} Largest Area : {area} Largest Perimeter : {perimeter}");
+            }
+        }
+    }
+}
